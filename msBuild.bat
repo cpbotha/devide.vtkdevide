913654d808f5c9258451451717c06f78ad2b3e6b
cpbotha <cpbotha@localhost>
2004-06-02 21:41:11 +0000

    * Added more information to README with regards to MSVS71 and especially DCMTK. * Fixed msBuild.bat.

--- msBuild.bat
+++ msBuild.bat
@@ -5,8 +5,8 @@ REM this is how we did it with VC6
 REM MSDEV.com VTKDEVIDE.dsw /MAKE "ALL_BUILD - Win32 RelWithDebInfo"
 
 REM this is how we do it with VS.NET 2003 (VS71)
-REM devenv ITK.sln /project ALL_BUILD /projectconfig "Debug|Win32" /build Debug
-devenv ITK.sln /project ALL_BUILD /projectconfig "RelWithDebInfo|Win32" /build RelWithDebInfo
+REM devenv vtkdevide.sln /project ALL_BUILD /projectconfig "Debug|Win32" /build Debug
+devenv vtkdevide.sln /project ALL_BUILD /projectconfig "RelWithDebInfo|Win32" /build RelWithDebInfo
 
 
 
