68f9e16179c15a61952b1eeab45ba368a7205bbe
cpbotha <cpbotha@localhost>
2004-06-02 15:39:07 +0000

    Changed build script for MS VS71.

--- msBuild.bat
+++ msBuild.bat
@@ -1,5 +1,12 @@
+@echo off
 REM build batch script for msdev under windows
-MSDEV.com VTKDEVIDE.dsw /MAKE "ALL_BUILD - Win32 RelWithDebInfo"
+
+REM this is how we did it with VC6
+REM MSDEV.com VTKDEVIDE.dsw /MAKE "ALL_BUILD - Win32 RelWithDebInfo"
+
+REM this is how we do it with VS.NET 2003 (VS71)
+REM devenv ITK.sln /project ALL_BUILD /projectconfig "Debug|Win32" /build Debug
+devenv ITK.sln /project ALL_BUILD /projectconfig "RelWithDebInfo|Win32" /build RelWithDebInfo
 
 
 
