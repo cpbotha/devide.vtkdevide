297c2017eb502b39937a9e8ac65860a846f34c28
cpbotha <cpbotha@localhost>
2004-11-01 11:09:05 +0000

    Added the external repo.

--- Wrapping/Python/vtkdevide.py
+++ Wrapping/Python/vtkdevide.py
@@ -2,7 +2,7 @@
 Code greatly simplified (more manual) so that McMillan installer has an
 easier time finding the imports.
 
-$Id: vtkdevide.py,v 1.2 2004/02/19 11:14:19 cpbotha Exp $
+$Id: vtkdevide.py,v 1.3 2004/11/01 11:09:05 cpbotha Exp $
 """
 
 import os
@@ -14,12 +14,14 @@ try:
         from libvtkdevideImagingPython import *
         from libvtkdevideIOPython import *
         from libvtkdevideRenderingPython import *
+        from libvtkdevideExternalPython import *
     else:
         from vtkdevideCommonPython import *        
         from vtkdevideHybridPython import *
         from vtkdevideImagingPython import *
         from vtkdevideIOPython import *
         from vtkdevideRenderingPython import *
+        from vtkdevideExternalPython import *
 except ImportError,e:
     print e
     pass
