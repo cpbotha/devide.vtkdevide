078386316ad70d18c77782e0a3567cbe432a5960
cpbotha <cpbotha@localhost>
2004-11-02 10:51:20 +0000

    Integrated vtkPVGlyphFilter.

---
+++ External/vtkPVGlyphFilter.cxx
@@ -0,0 +1,94 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    $RCSfile: vtkPVGlyphFilter.cxx,v $
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVGlyphFilter.h"
+
+#include "vtkMaskPoints.h"
+#include "vtkObjectFactory.h"
+#include "vtkPolyData.h"
+
+vtkCxxRevisionMacro(vtkPVGlyphFilter, "$Revision: 1.1 $");
+vtkStandardNewMacro(vtkPVGlyphFilter);
+
+vtkPVGlyphFilter::vtkPVGlyphFilter()
+{
+  this->SetColorModeToColorByScalar();
+  this->SetScaleModeToScaleByVector();
+  this->MaskPoints = vtkMaskPoints::New();
+  this->MaximumNumberOfPoints = 5000;
+  this->UseMaskPoints = 1;
+}
+
+vtkPVGlyphFilter::~vtkPVGlyphFilter()
+{
+  this->MaskPoints->Delete();
+}
+
+void vtkPVGlyphFilter::SetInput(vtkDataSet *input)
+{
+  this->MaskPoints->SetInput(input);
+  this->Superclass::SetInput(this->MaskPoints->GetOutput());
+}
+
+void vtkPVGlyphFilter::SetRandomMode(int mode)
+{
+  this->MaskPoints->SetRandomMode(mode);
+}
+
+int vtkPVGlyphFilter::GetRandomMode()
+{
+  return this->MaskPoints->GetRandomMode();
+}
+
+void vtkPVGlyphFilter::Execute()
+{
+  if (this->UseMaskPoints)
+    {
+    this->Superclass::SetInput(this->MaskPoints->GetOutput());
+    vtkIdType numPts = this->MaskPoints->GetInput()->GetNumberOfPoints();
+    vtkIdType maxNumPts =
+      this->MaximumNumberOfPoints; //  / this->NumberOfProcesses;
+    maxNumPts = (maxNumPts < 1) ? 1 : maxNumPts;
+    this->MaskPoints->SetMaximumNumberOfPoints(maxNumPts);
+    this->MaskPoints->SetOnRatio(numPts / maxNumPts);
+    this->MaskPoints->Update();
+    }
+  else
+    {
+    this->Superclass::SetInput(this->MaskPoints->GetInput());
+    }
+  
+  this->Superclass::Execute();
+}
+
+void vtkPVGlyphFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+  os << indent << "InputScalarsSelection: " 
+     << (this->InputScalarsSelection ? this->InputScalarsSelection : "(none)")
+     << endl;
+
+  os << indent << "InputVectorsSelection: " 
+     << (this->InputVectorsSelection ? this->InputVectorsSelection : "(none)")
+     << endl;
+
+  os << indent << "InputNormalsSelection: " 
+     << (this->InputNormalsSelection ? this->InputNormalsSelection : "(none)")
+     << endl;
+  
+  os << indent << "MaximumNumberOfPoints: " << this->GetMaximumNumberOfPoints()
+     << endl;
+
+  os << indent << "UseMaskPoints: " << (this->UseMaskPoints?"on":"off") << endl;
+}
---
+++ External/vtkPVGlyphFilter.h
@@ -0,0 +1,66 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    $RCSfile: vtkPVGlyphFilter.h,v $
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPVGlyphFilter -
+#ifndef __vtkPVGlyphFilter_h
+#define __vtkPVGlyphFilter_h
+
+#include "vtkGlyph3D.h"
+
+class vtkMaskPoints;
+
+class VTK_EXPORT vtkPVGlyphFilter : public vtkGlyph3D
+{
+public:
+  vtkTypeRevisionMacro(vtkPVGlyphFilter,vtkGlyph3D);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description
+  static vtkPVGlyphFilter *New();
+
+  // Description:
+  // Limit the number of points to glyph
+  vtkSetMacro(MaximumNumberOfPoints, int);
+  vtkGetMacro(MaximumNumberOfPoints, int);
+
+  // Description:
+  // Set the input to this filter.
+  virtual void SetInput(vtkDataSet *input);
+  
+  // Description:
+  // Set/get whether to mask points
+  vtkSetMacro(UseMaskPoints, int);
+  vtkGetMacro(UseMaskPoints, int);
+
+  // Description:
+  // Set/get flag to cause randomization of which points to mask.
+  void SetRandomMode(int mode);
+  int GetRandomMode();
+
+protected:
+  vtkPVGlyphFilter();
+  ~vtkPVGlyphFilter();
+
+  virtual void Execute();
+  
+  vtkMaskPoints *MaskPoints;
+  int MaximumNumberOfPoints;
+  int UseMaskPoints;
+  
+private:
+  vtkPVGlyphFilter(const vtkPVGlyphFilter&);  // Not implemented.
+  void operator=(const vtkPVGlyphFilter&);  // Not implemented.
+};
+
+#endif
