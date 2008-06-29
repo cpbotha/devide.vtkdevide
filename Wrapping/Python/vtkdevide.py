2270e770698a838ccfb842f61e0028eedcb39431
cpbotha <cpbotha@localhost>
2008-06-29 19:43:25 +0000

    Added dynamic loading configuration code, taken from VTK.

--- Wrapping/Python/vtkdevide.py
+++ Wrapping/Python/vtkdevide.py
@@ -2,19 +2,50 @@
 Code greatly simplified (more manual) so that McMillan installer has an
 easier time finding the imports.
 
-$Id: vtkdevide.py,v 1.3 2004/11/01 11:09:05 cpbotha Exp $
+$Id$
 """
 
 import os
 
 try:
     if os.name == 'posix':
+
+        # took DL handling from Mathieu Malaterre's vtkgdcm.py
+
+        # extremely important !
+        # http://gcc.gnu.org/faq.html#dso
+        # http://mail.python.org/pipermail/python-dev/2002-May/023923.html
+        # http://wiki.python.org/moin/boost.python/CrossExtensionModuleDependencies
+        # This is now merged in VTK 5.2:
+        # http://vtk.org/cgi-bin/viewcvs.cgi/Wrapping/Python/vtk/__init__.py?r1=1.13&r2=1.14
+        import sys
+        orig_dlopen_flags = sys.getdlopenflags()
+        try:
+          import dl
+        except ImportError:
+          # are we on AMD64 ?
+          try:
+            import DLFCN as dl
+          except ImportError:
+            print "Could not import dl"
+            dl = None
+        if dl:
+          #print "dl was imported"
+          #sys.setdlopenflags(dl.RTLD_LAZY|dl.RTLD_GLOBAL)
+          sys.setdlopenflags(dl.RTLD_NOW|dl.RTLD_GLOBAL)
+  
         from libvtkdevideCommonPython import *        
         from libvtkdevideHybridPython import *
         from libvtkdevideImagingPython import *
         from libvtkdevideIOPython import *
         from libvtkdevideRenderingPython import *
         from libvtkdevideExternalPython import *
+
+        # revert:
+        sys.setdlopenflags(orig_dlopen_flags)
+        del sys, dl
+        del orig_dlopen_flags
+  
     else:
         from vtkdevideCommonPython import *        
         from vtkdevideHybridPython import *
